367354c6d98a71d13229ab704194a9b21ada2509
dennisDev77 <diniscamela5@gmail.com>
2024-12-05 11:09:15 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <div>
+      <h1 className='text-blue-500 text-2xl'>Bem Vindo ao Ativa Recargas</h1>
+  </div>
+    </>
+  )
+}
+
+export default App

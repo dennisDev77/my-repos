27b5f39fe1026ea425067c62e319f12d5c3f39ee
dennisDev77 <diniscamela5@gmail.com>
2025-02-11 11:07:01 +0100

    Add end to link

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <section className='py-5 flex flex-wrap justify-center items-center sticky top-0 left-0'>
-       <Link className='text-color-white bg-color-blue  p-2 rounded-md z-20' to='/' end>Home</Link>
+       <Link className='text-color-white bg-color-blue  p-2 rounded-md z-20' to='/' end='true'>Home</Link>
     </section>
   )
 }
